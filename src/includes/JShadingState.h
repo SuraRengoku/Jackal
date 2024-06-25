dd09f8268d5037585273e74a650708bc13038ec7
SuraRengoku <jiahao.liu@rwth-aachen.de>
2024-06-25 13:31:39 +0200

    Texture part1

---
+++ src/includes/JShadingState.h
@@ -0,0 +1,8 @@
+﻿//
+// Created by jonas on 2024/6/25.
+//
+
+#ifndef JSHADINGSTATE_H
+#define JSHADINGSTATE_H
+
+#endif //JSHADINGSTATE_H
---
+++ src/includes/JTextureHolder.h
@@ -0,0 +1,8 @@
+﻿//
+// Created by jonas on 2024/6/25.
+//
+
+#ifndef JTEXTUREHOLDER_H
+#define JTEXTUREHOLDER_H
+
+#endif //JTEXTUREHOLDER_H
